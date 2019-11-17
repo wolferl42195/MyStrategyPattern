e768c12e0a1d84f457b1fc1ed57e55e80961bbfd
dominik polzer <dominik.polzer1990@gmail.com>
2019-11-17 14:04:04 +0100

    created polzer class

---
+++ src/main/java/at/ac/fhcampuswien/students/Polzer.java
@@ -0,0 +1,15 @@
+package at.ac.fhcampuswien.students;
+
+import at.ac.fhcampuswien.Student;
+
+public class Polzer extends Student {
+
+	public String getName() {
+		return "Polzer Dominik";
+	}
+	
+	public String getMyFavoriteQuote() {
+		return "Smile, because it confuses people";
+	}
+
+}
